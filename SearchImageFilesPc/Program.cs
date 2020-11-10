7c04f262ae89302196e0c03589a4fe4dae1ac4a2
Test Testsen <sadsafefs84fe6a4f5saf5s@outlook.dk>
2020-11-10 11:01:09 +0100

    Add project files.

---
+++ SearchImageFilesPc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace SearchImageFilesPc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchFolders searchFolders = new SearchFolders();
+            searchFolders.StartScanning(@"C:\Users");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n");
+            Console.WriteLine("!--------------------------------------------------------------------------!\n");
+            Console.WriteLine("Billeder fundet: " + searchFolders._imageCount);
+            Console.WriteLine("\n!--------------------------------------------------------------------------!");
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+            Thread.Sleep(5000);
+            foreach (var item in searchFolders.imageList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ SearchImageFilesPc/SearchFolders.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchImageFilesPc
+{
+    public class SearchFolders
+    {
+        //private string folderPath = @"C:\";
+
+        string[] ImageFormatArray = new string[] { "jpg", "jpeg", "png", "gif", "eps", "raw", "cr2", "crw", "nef", "pef", "tiff" };
+
+        public int _imageCount;
+
+        public List<string> imageList;
+
+        public SearchFolders()
+        {
+            _imageCount = 0;
+            imageList = new List<string>();
+        }
+
+        public void StartScanning(string folderPath)
+        {
+            Console.WriteLine("Scanning Startet.\n");
+            FolderScan(folderPath);
+        }
+
+        private void FolderScan(string folderPath)
+        {
+            var allFolders = Directory.GetFileSystemEntries(folderPath);
+
+            foreach (var item in allFolders)
+            {
+                try
+                {
+                    if (!item.ToLower().Contains("\\appdata"))
+                    {
+                        var test = Path.GetExtension(item).Split('.').Last();
+                        foreach (var format in ImageFormatArray)
+                        {
+                            if (test.Contains(format))
+                            {
+                                _imageCount += 1;
+                                imageList.Add(item);
+                            }
+                        }
+                    }
+
+                    if (Directory.Exists(item))
+                    {
+                        Console.WriteLine(item);
+                        FolderScan(item);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(e.Message);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+    }
+}
